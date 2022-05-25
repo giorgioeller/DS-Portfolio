36da9d1d21eda3288512eb82d70c5c0ebd478a1b
Giorgio Eller dos Santos <giorgio_eller@hotmail.com>
2022-05-25 15:35:32 -0300

    Improved filters
    Added a filter for country selection and small design changes.

--- Country Logistic Data Visualization/app.py
+++ Country Logistic Data Visualization/app.py
@@ -1,7 +1,7 @@
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
-# assume you have a "long-form" data frame
+# assume you have a 'long-form' data frame
 # see https://plotly.com/python/px-arguments/ for more options
 
 # continent data obtained from https://www.geonames.org/countries/
@@ -16,21 +16,23 @@ import pandas as pd
 app = Dash(__name__)
 
 # Load logistic and continent data
-continents = pd.read_csv("continents.csv", delimiter=";", na_filter=False)
-continents = continents.replace({"Continent":{"AS":"Asia", "EU":"Europe", "AF":"Africa", "OC":"Oceania",
-                                              "NA":"North America", "SA":"South America"}})
-df = pd.read_csv("logistics_data.csv")
+continents = pd.read_csv('continents.csv', delimiter=';', na_filter=False)
+continents = continents.replace({'Continent':{'AS':'Asia', 'EU':'Europe', 'AF':'Africa', 'OC':'Oceania',
+                                              'NA':'North America', 'SA':'South America'}})
+df = pd.read_csv('logistics_data.csv')
 
 # Create a dataframe with the raw data
-df = df.merge(continents, on="Country Code", how="left")
+df = df.merge(continents, on='Country Code', how='left')
 df = df.dropna(subset=['Country Code'])
 
 # Variables with data for dataframe about logistic data
-countries = df["Country Name"]
-dates = [2007, 2010, 2012, 2014, 2016, 2018]
+countries = df['Country Name']
 logistic_values = df.iloc[:, 4:10].transpose()
-logistic_values = logistic_values.replace(to_replace="..", value=None)
+logistic_values = logistic_values.replace(to_replace='..', value=None)
 logistic_values = logistic_values.astype(float)
+dates = []
+for i in logistic_values.index.values.tolist():
+    dates.append(int(i[0:4]))
 
 # Create dataframe for logistic data 
 logistic_data = pd.DataFrame(logistic_values.values, columns=countries)
@@ -43,32 +45,49 @@ app.layout = html.Div(children=[
         Sorted by country and/or continent.
     '''),
 
-    # Graph and checklist
+    # Interactive plot and filters
     html.Div(children=[
         dcc.Graph(
             id='logistics-graph',
-            style={'width': '170vh', 'height': '100vh'}
+            style={'width': '80%', 'height': '75vh', 'display': 'inline-block'}
             ),
 
         dcc.Checklist(
-            id="continent-checklist",
-            options=["Africa", "North America", "South America", "Asia", "Europe", "Oceania"],
-            value=["North America"],
-            inline=False
+            id='continent-checklist',
+            options=['Africa', 'North America', 'South America', 'Asia', 'Europe', 'Oceania'],
+            value=['North America'],
+            labelStyle = {'display': 'block'},
+            style={'width': '19%', 'vertical-align': 'top', 'display': 'inline-block'}
             )
     ]),
-    
+
+    html.Div(children=
+        dcc.Dropdown(
+            id='country-dropdown',
+            multi=True
+        )
+    )
 ])
 
-# Callback for updating graph
+# Callback for updating available countries
 @app.callback(
-    Output("logistics-graph", "figure"),
-    Input("continent-checklist", "value"))
-def update_graph(continent_selected):
-    selected_countries = df.loc[df["Continent"].isin(continent_selected), 'Country Name'].values.tolist()
-    fig = px.line(logistic_data[selected_countries], 
+    Output('country-dropdown', 'options'),
+    Input('continent-checklist', 'value'))
+def update_dropdown(available_countries):
+    selected_countries = df.loc[df['Continent'].isin(available_countries), 'Country Name'].values.tolist()
+    return selected_countries
+
+@app.callback(
+    Output('logistics-graph', 'figure'),
+    Input('country-dropdown', 'value'))
+def update_graph(countries):
+    if countries is not None:
+        shown_countries = countries
+    else:
+        shown_countries = []
+    fig = px.line(logistic_data[shown_countries], 
                   x=dates, 
-                  y=logistic_data[selected_countries].columns[:], 
+                  y=logistic_data[shown_countries].columns[:], 
                   markers=True)
     return fig
 
